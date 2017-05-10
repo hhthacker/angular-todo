ad51685dad7a652e631e54eb4d52f145274fc906
Heather Thacker <hhthacker@gmail.com>
2017-05-09 20:49:55 -0500

    button toggle

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,23 @@
-console.log("hey");
+var app = angular.module("TodoApp", []);
+
+
+app.controller("NavCtrl", ($scope) => {
+	$scope.cat = "Meow";
+	$scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name: "New Item"}];
+});
+
+
+app.controller("ItemCtrl", ($scope) => {
+	$scope.dog = "Woof";
+	$scope.showListView = true;
+
+	$scope.newItem = () => {
+		$scope.showListView = false;
+		};
+
+	$scope.allItem = () => {
+		$scope.showListView = true;
+	};
+
+
+});
