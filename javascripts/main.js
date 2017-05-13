5d43a06d0363fb3a3447d810283e7d0fc778de4e
Heather Thacker <hhthacker@gmail.com>
2017-05-13 11:42:51 -0500

    firebase data

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,23 +1,56 @@
-var app = angular.module("TodoApp", []);
-
-
-app.controller("NavCtrl", ($scope) => {
-	$scope.cat = "Meow";
-	$scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name: "New Item"}];
+app.run((FIREBASE_CONFIG) => {
+    firebase.initializeApp(FIREBASE_CONFIG);
 });
 
-app.controller("ItemCtrl", ($scope) => {
-	$scope.dog = "Woof";
-	$scope.showListView = true;
-	$scope.items = [];
+app.controller("NavCtrl", ($scope) => {
+    $scope.cat = "Meow";
+    $scope.navItems = [{ name: "Logout" }, { name: "All Items" }, { name: "New Item" }];
+});
 
-	$scope.newItem = () => {
-		$scope.showListView = false;
-		};
+app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
+    $scope.dog = "Woof";
+    $scope.showListView = true;
+    $scope.items = [];
 
-	$scope.allItem = () => {
-		$scope.showListView = true;
-	};
+    $scope.newItem = () => {
+        $scope.showListView = false;
+    };
+
+    $scope.allItem = () => {
+        $scope.showListView = true;
+    };
+
+    let getItemList = () => {
+        let itemz = [];
+        return $q((resolve, reject) => {
+            $http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)
+                .then((fbItems) => {
+                    var itemCollection = fbItems.data;
+                    Object.keys(itemCollection).forEach((key) => {
+                        itemCollection[key].id = key;
+                        itemz.push(itemCollection[key]);
+                    });
+                    resolve(itemz);
+                })
+                .catch((error) => {
+                    reject(error);
+                });
+        });
+    };
+
+    let getItems = () => {
+        getItemList().then((itemz) => {
+            console.log("itemz", itemz);
+        }).catch((error) => {
+            console.log("get Error", error);
+        });
+    };
+
+    getItems();
+
+
+
+
 
 
 });
