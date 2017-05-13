77d54cdadf6f79a699af00c46006ca9ace78ea7e
Heather Thacker <hhthacker@gmail.com>
2017-05-13 10:51:08 -0500

    modules

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,32 +6,10 @@ app.controller("NavCtrl", ($scope) => {
 	$scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name: "New Item"}];
 });
 
-
 app.controller("ItemCtrl", ($scope) => {
 	$scope.dog = "Woof";
 	$scope.showListView = true;
-	$scope.items = [
-        {
-          id: 0,
-          task: "mow the lawn",
-          isCompleted: true,
-          assignedTo: "Callan",
-        },
-        {
-          id: 1,
-          task: "grade quizzes",
-          isCompleted: false,
-          assignedTo: "Lauren",
-        },
-        {
-          id: 2,
-          task: "take a nap",
-          isCompleted: false,
-          assignedTo: "Zoe",
-        }
-      ];
-
-
+	$scope.items = [];
 
 	$scope.newItem = () => {
 		$scope.showListView = false;
