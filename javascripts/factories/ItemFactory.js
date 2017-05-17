36cedacc05ac7215dc240f53c937f1886f4cd5c8
Heather Thacker <hhthacker@gmail.com>
2017-05-16 20:04:44 -0500

    delete

--- javascripts/factories/ItemFactory.js
+++ javascripts/factories/ItemFactory.js
@@ -5,10 +5,12 @@ app.factory("ItemFactory", function($http, $q, FIREBASE_CONFIG) {
             $http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)
                 .then((fbItems) => {
                     var itemCollection = fbItems.data;
+                    if(itemCollection.length !== null ) {
                     Object.keys(itemCollection).forEach((key) => {
                         itemCollection[key].id = key;
                         itemz.push(itemCollection[key]);
                     });
+                }
                     resolve(itemz);
                 })
                 .catch((error) => {
@@ -28,5 +30,28 @@ app.factory("ItemFactory", function($http, $q, FIREBASE_CONFIG) {
         });
     };
 
-    return { getItemList:getItemList, postNewItem:postNewItem };
+    let deletz = (itemId) => {
+        return $q((resolve, reject) => {
+            $http.delete(`${FIREBASE_CONFIG.databaseURL}/items/${itemId}.json`)
+                .then((resultz) => {
+                    resolve(resultz);
+                }).catch((error) => {
+                    reject(error);
+                });
+        });
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return {getItemList:getItemList, postNewItem:postNewItem, deletz:deletz};
 });
